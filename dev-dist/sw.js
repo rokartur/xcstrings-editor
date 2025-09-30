ad135212856d2cb1fec1ef996fefcf992c1423c8
rokartur <rokartur@icloud.com>
2025-10-01 00:45:49 +0200

    fix: icon colors in header

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.atr7mbp9uog"
+    "revision": "0.vg8icp6ur1s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
